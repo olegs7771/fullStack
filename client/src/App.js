78d7f0da149e9f327ec3b95d86d2133b1006bc7e
Oleg Smushkevich <olegs777@bezeqint.net>
2018-12-29 22:21:22 +0200

    working on Main background

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="container">
+          <Main />
+        </div>
       </div>
     );
   }
---
+++ client/src/components/layout/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <div className="display-4 mb-4">Developer Connector</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Main;
