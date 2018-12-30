565c9c760266e08fdade8f3bdbf941f3a1d1318d
oleg <olegs777@bezeqint.net>
2018-12-30 15:13:01 +0200

    worked on Register.js form with state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Register extends Component {
+  state = {
+    name: "",
+    email: "",
+    password1: "",
+    password2: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
+    const { name, email, password1, password2 } = this.state;
     return (
       <div className="register">
         <div className="row">
@@ -18,8 +32,42 @@ class Register extends Component {
                         type="text"
                         className="form-control"
                         name="name"
-                        value=""
+                        value={name}
                         placeholder="Name..."
+                        onChange={this.onChange}
+                      />
+                    </div>
+                    <div className="group-control group-control-lg">
+                      <label>Email</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        value={email}
+                        placeholder="Name..."
+                        onChange={this.onChange}
+                      />
+                    </div>
+                    <div className="group-control group-control-lg">
+                      <label>Password</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="password1"
+                        value={password1}
+                        placeholder="Password..."
+                        onChange={this.onChange}
+                      />
+                    </div>
+                    <div className="group-control group-control-lg">
+                      <label>Confirm Password</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="password2"
+                        value={password2}
+                        placeholder="Confirm Password..."
+                        onChange={this.onChange}
                       />
                     </div>
                   </form>
