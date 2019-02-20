93c44643dfd74144c6193a0362eaa1f57a668635
Oleg Smushkevich <olegs777@bezeqint.net>
2019-02-20 22:16:32 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport"); //authantication strategy
+const path = require("path");
 
 //routes imports
 const users = require("./routes/api/users");
@@ -19,10 +20,7 @@ const db = require("./config/keys").mongodb;
 // Conect to mongoDB
 
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -33,6 +31,15 @@ app.use(passport.initialize());
 app.use("/api/posts", posts);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
+
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
